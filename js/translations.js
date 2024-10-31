318b3f30905978ab4dd7e4c133a7a436f3b1782f
Mouhamed DIOP <mouhamed.diop@esp.sn>
2024-10-31 16:13:09 +0000

    Put links to all versions of documents
    - Put links to both english and french versions of documents
    - Reorganize team members aparition order
    - Remove link from document titles

--- js/translations.js
+++ js/translations.js
@@ -1,9 +1,9 @@
 function makeIconsAreaClickable() {
 	let iconElement;
-	const links = document.querySelectorAll('div.p-4 i + h5.mb-3 > a');
+	const links = document.querySelectorAll('div.p-4 > p.version-box > a:first-of-type');
 
 	for (const link of links) {
-		iconElement = link.parentElement.previousElementSibling;
+		iconElement = link.parentElement.parentElement.firstElementChild;
 		iconElement.style.cursor = 'pointer';
 		iconElement.onclick = (e) => {
 			if (e.target.tagName != 'A') {
