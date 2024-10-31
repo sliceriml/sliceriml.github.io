7007e8437675302ece974f89a1edaa84ec293a56
Mouhamed DIOP <mouhamed.diop@esp.sn>
2024-10-31 01:49:40 +0000

    Integrate Sonia's last update requests
    - Add Acknowledgement section
    - Make simpler the Events menu item
    - Change some menu items title
    - Organize Team alumni by alphabetic order

--- js/translations.js
+++ js/translations.js
@@ -1,11 +1,15 @@
 function makeIconsAreaClickable() {
-	let parent;
+	let iconElement;
 	const links = document.querySelectorAll('div.p-4 i + h5.mb-3 > a');
 
 	for (const link of links) {
-		parent = link.parentElement.parentElement.parentElement.parentElement;
-		parent.onclick = () => link.click();
-		parent.style.cursor = 'pointer';
+		iconElement = link.parentElement.previousElementSibling;
+		iconElement.style.cursor = 'pointer';
+		iconElement.onclick = (e) => {
+			if (e.target.tagName != 'A') {
+				link.click(e);
+			}
+		}
 	}
 }
 
